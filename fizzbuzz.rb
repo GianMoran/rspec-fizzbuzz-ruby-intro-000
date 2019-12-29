8ea04806ad4e0bb713d0f29894c98e93866de834
Gian Moran <gpmoran_@outlook.com>
2019-12-29 23:56:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,15 +2,14 @@
 # See README.md for instructions on how to do this
 require 'pry'
 def fizzbuzz(int)
+   bindgin.pry
   if int % 3 == 0 
     return "Fizz"
- bindgin.pry
   elsif int % 5 == 0
     return "Buzz"
   end
   if int % 3 == 0 && int % 5 == 0
     return "Fizzbuzz"
-    
   end
   return nil
 end
