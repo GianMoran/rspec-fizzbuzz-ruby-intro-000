18de445a11cc95c5b61b1e10440190d9406e5546
Gian Moran <gpmoran_@outlook.com>
2019-12-29 23:45:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,15 @@
 # See README.md for instructions on how to do this
 require_relative './spec_helper.rb'
 
-def fizbuzz 
+def fizbuzz(int)
+  if int % 3 == 0 
+    return "Fizz"
+  end
+  if int % 5 == 0
+    return "Buzz"
+  end
+  if int % 3 == 0 && int % 5 = 0
+    return "Fizzbuzz"
+  end
   
 end
